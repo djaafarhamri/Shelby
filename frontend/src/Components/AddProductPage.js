73a4da5f3f5e123e7b0bd3450e07db51d59079c3
djaafarhamri <hamridjaafar12@gmail.com>
2022-01-17 23:34:03 -0800

    add product skeleton

--- frontend/src/Components/AddProductPage.js
+++ frontend/src/Components/AddProductPage.js
@@ -1,9 +1,54 @@
-import './AddProductPage.css'
+import "./AddProductPage.css";
+import { useState } from 'react'
 
-const AddProductPage = () => {
-    return ( 
-        <div className="add-product-page"></div>
-     );
-}
- 
+const AddProductPage = ({ setSelected }) => {
+    const [nom, setNom] = useState('')
+    const [descr, setsetDescr] = useState('')
+    const [prixAch, setPrixAch] = useState('')
+    const [prix, setPrix] = useState('')
+    const [marque, setMarque] = useState('')
+    const [genre, setGenre] = useState('')
+    const [categorie, setCategorie] = useState('')
+    const [subCategorie, setSubCategorie] = useState('')
+    const [ref, setRef] = useState('')
+  return (
+    <div className="add-product-page">
+      <div className="add-up">
+        <button
+          onClick={() => {
+            setSelected("m");
+          }}
+        >
+          back
+        </button>
+        <h2>Ajouter un Produit</h2>
+      </div>
+      <div className="add-form">
+        <p>Nom</p>
+        <input type="text" onChange={(e) => {setNom(e.target.value)}}/>
+        <p>Description</p>
+        <input type="text" onChange={(e) => {setsetDescr(e.target.value)}}/>
+        <p>Prix d'achat</p>
+        <input type="text" onChange={(e) => {setPrixAch(e.target.value)}}/>
+        <p>Prix de vente</p>
+        <input type="text" onChange={(e) => {setPrix(e.target.value)}}/>
+        <p>Marque</p>
+        <input type="text" onChange={(e) => {setMarque(e.target.value)}}/>
+        <p>Genre</p>
+        <select id="Genre" name="Genre" onChange={(e) => {setGenre(e.target.value)}}>
+          <option value="australia">Classic</option>
+          <option value="canada">Sport</option>
+          <option value="Semi-Classic">Semi-Classic</option>
+        </select>
+        <p>Categorie</p>
+        <input type="text" onChange={(e) => {setCategorie(e.target.value)}}/>
+        <p>Sub-Categorie</p>
+        <input type="text" onChange={(e) => {setSubCategorie(e.target.value)}}/>
+        <p>Reference</p>
+        <input type="text" onChange={(e) => {setRef(e.target.value)}}/>
+      </div>
+    </div>
+  );
+};
+
 export default AddProductPage;
--- frontend/src/Components/Stock.js
+++ frontend/src/Components/Stock.js
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import './Stock.css'
+import "./Stock.css";
+import AddProductPage from "./AddProductPage";
 
 const ENDPOINT = "http://localhost:4000";
 
 const Stock = () => {
   const [products, setProducts] = useState([]);
+  const [selected, setSelected] = useState("m");
   useEffect(() => {
     axios
       .get(`${ENDPOINT}/api/getAllProducts`)
@@ -15,31 +17,43 @@ const Stock = () => {
       .catch((err) => console.log(err));
   }, []);
   return (
-    <div className="stock">
-      <div className="stock-search">
-        <input type="text" placeholder="Search here" />
-      </div>
-      <div className="table">
-        <h2>All Products</h2>
-        <div className="stock-table">
-          <h3 className="stock-name">Name</h3>
-          <h3 className="stock-ref">Ref</h3>
-          <h3 className="stock-taille">Taille</h3>
-          <h3 className="stock-quantity">Quantity</h3>
-          <h3 className="stock-prix">Prix</h3>
-          {products &&
-            products.map((product, index) => (
-              <div key={index} className="stock-admin">
-                <p className="stock-name">{product.username}</p>
-                <p className="stock-ref">{product.ref}</p>
-                <p className="stock-taille">{product.taille}</p>
-                <p className="stock-quantity">{product.quantity}</p>
-                <p className="stock-prix">{product.price}</p>
-              </div>
-            ))}
+    <>
+      {selected === "m" && (
+        <div className="stock">
+          <div className="stock-search">
+            <input type="text" placeholder="Search here" />
+          </div>
+          <button
+            onClick={() => {
+              setSelected("a");
+            }}
+          >
+            ajouter
+          </button>
+          <div className="table">
+            <h2>All Products</h2>
+            <div className="stock-table">
+              <h3 className="stock-name">Name</h3>
+              <h3 className="stock-ref">Ref</h3>
+              <h3 className="stock-taille">Taille</h3>
+              <h3 className="stock-quantity">Quantity</h3>
+              <h3 className="stock-prix">Prix</h3>
+              {products &&
+                products.map((product, index) => (
+                  <div key={index} className="stock-admin">
+                    <p className="stock-name">{product.username}</p>
+                    <p className="stock-ref">{product.ref}</p>
+                    <p className="stock-taille">{product.taille}</p>
+                    <p className="stock-quantity">{product.quantity}</p>
+                    <p className="stock-prix">{product.price}</p>
+                  </div>
+                ))}
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+      {selected === "a" && <AddProductPage setSelected={setSelected} />}
+    </>
   );
 };
 
