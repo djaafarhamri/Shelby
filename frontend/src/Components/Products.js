f73cb049497ad62a5570f0c940763015046a0b64
mekkaoui7 <ahmedmike350@gmail.com>
2022-01-21 19:10:43 +0000

    shadow

--- frontend/src/Components/Products.js
+++ frontend/src/Components/Products.js
@@ -2,7 +2,7 @@ import Product from "./Product";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Navbar from "./Navbar";
-import pic from "../assets/hero.png";
+import pic from "../assets/jordan.jpg";
 const ENDPOINT = "http://localhost:4000";
 
 const Products = () => {
@@ -51,13 +51,26 @@ const Products = () => {
          </div>
        </div>
  
-      <div className="mt-6 grid grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-x-3 gap-y-5">
-        <div className="">
-          <img className="object-center object-cover lg:w-full lg:h-full" src={pic} alt="" />
-          <p className="font-bold font-monteserrat text-xl">Mark of product</p>
-          <p className="font-monteserrat">little details</p>
-          <p className="font-bold font-monteserrat text-center">Price</p>
-        </div>
+      <div className="mt-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-3 gap-y-5">
+        <div className="group relative rounded-md shadow-2xl ">
+              <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 ">
+                <img
+                  src={pic}
+                  alt=""
+                  className="w-full h-full object-center object-cover lg:w-full lg:h-full xl:w-full xl:h-full"
+                />
+              </div>
+              <div className="mt-4  ">
+                <div>
+                  <h3 className="text-xl font-bold text-center font-monteserrat">                   
+                      Jordan Air force
+                  </h3>
+                  <p className="mt-1 pr-1 text-md font-monteserrat">description de produit</p>
+                  <p className="text-lg font-extrabold text-center font-monteserrat">18000DA</p>
+                </div>
+                
+              </div>
+            </div>
       </div>
     </div>
   );
