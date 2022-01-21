4e5ad37551063d081a7b603508131e9d04569a6c
mekkaoui7 <ahmedmike350@gmail.com>
2022-01-21 18:12:24 +0000

    products ongoing

--- frontend/src/Components/Products.js
+++ frontend/src/Components/Products.js
@@ -1,8 +1,8 @@
 import Product from "./Product";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import "./Products.css";
 import Navbar from "./Navbar";
+import pic from "../assets/hero.png";
 const ENDPOINT = "http://localhost:4000";
 
 const Products = () => {
@@ -19,11 +19,11 @@ const Products = () => {
   const [filter2,isopen2]=useState(false);
   const [filter3,isopen3]=useState(false);
   return (
-    <div className="products-page">
+    <div className="">
       <Navbar />
 
-      <div className="flex border-b-2 border-black justify-around">
-         <div className="flex-auto relative">
+      <div className='flex border-b-2 border-black justify-around'>
+         <div className=" relative">
            <p onClick={()=>isopen1(!filter1)} className="font-monteserrat text-2xl" > Categorie</p>
            {filter1 && <ul className="absolute">
              <li>Classic</li>
@@ -31,7 +31,7 @@ const Products = () => {
              <li></li>
            </ul>}
          </div>
-         <div className="flex-auto relative">
+         <div className=" relative">
            <p onClick={()=>isopen2(!filter2)} className="font-monteserrat text-2xl"> Pointure</p>
            {filter2 && <ul className="absolute">
              <li>Classic</li>
@@ -40,7 +40,7 @@ const Products = () => {
            </ul>}
 
          </div>
-         <div className="flex-auto relative">
+         <div className=" relative">
            <p onClick={()=>isopen3(!filter3)} className="font-monteserrat text-2xl"> Marque</p>
            {filter3 && <ul className="absolute">
              <li>Classic</li>
@@ -51,13 +51,13 @@ const Products = () => {
          </div>
        </div>
  
-      <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2">
-        {products &&
-          products.map((product, index) => (
-            <div key={index} className="products-map">
-              <Product title={product.title} marque={product.marque} />
-            </div>
-          ))}
+      <div className="mt-6 grid grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-x-3 gap-y-5">
+        <div className="">
+          <img className="object-center object-cover lg:w-full lg:h-full" src={pic} alt="" />
+          <p className="font-bold font-monteserrat text-xl">Mark of product</p>
+          <p className="font-monteserrat">little details</p>
+          <p className="font-bold font-monteserrat text-center">Price</p>
+        </div>
       </div>
     </div>
   );
