3c97e2b74e672f2472a8db974b0fc5121480f8f6
mekkaoui7 <ahmedmike350@gmail.com>
2022-01-24 22:26:28 +0000

    Home

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,4 +1,5 @@
 import hero from'../assets/home-img.png';
+
 import { useContext } from 'react';
 import vest from'../assets/vest.JPG';
 import sport from'../assets/sport.JPG';
@@ -12,14 +13,13 @@ const Home = () => {
    
   <div>
      {<Navbar />}
-  <div className='relative '>
-    <img className='h-[calc(100vh-4rem)] w-[calc(100%)]' src={hero} alt="" />
-    <h1 className='text-4xl sm:text-6xl text-palete absolute -top-0 my-20 mx-8 font-montserrat'>Bienvenue,<br/> Dans shelby Boutique</h1>
-    <div className='absolute -bottom-0 right-0 mr-12 mb-8 flex'>
-    <h1 className=' text-palete font-montserrat text-4xl pr-2 sm:text-5xl'>Enjoy</h1>
-    <svg className="w-9 h-11 animate-bounce sm:w-12 sm:h-15" fill="none" stroke="white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13l-3 3m0 0l-3-3m3 3V8m0 13a9 9 0 110-18 9 9 0 010 18z"></path></svg>
-    </div>
-  </div>
+     <div className=' font-monteserrat'>
+       <img className='h-[calc(100vh-4rem)] w-[calc(100%)] relative object-cover' src={hero} alt="" />
+       <h1 className='absolute top-1/3 left-1/2 transform -translate-x-1/2 text-3xl sm:hidden text-palete'>Bienvenue De Votre Boutique </h1>
+       <h1 className='hidden sm:block sm:text-4xl lg:text-6xl  absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-palete'>Bienvenue  </h1>
+       <h1 className='hidden sm:block sm:text-4xl lg:text-5xl absolute  top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-palete'>Dand Votre Boutique  </h1>
+       <button className='absolute text-2xl sm:text-3xl bg-palete bottom-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-2xl py-2 px-2'>découvrir</button>
+     </div>
   <div className=' grid  grid-cols-1 auto-cols-auto  sm:grid-cols-3 h-screen text-palete font-montserrat'>
     <div className='relative '>
     <img className='object-cover object-center w-full h-full' src={classic} alt="" />
