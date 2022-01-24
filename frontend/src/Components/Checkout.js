8faf7a5b635ad4c456190145249424565499e666
mekkaoui7 <ahmedmike350@gmail.com>
2022-01-24 13:55:41 +0000

    chekout mobile

--- frontend/src/Components/Checkout.js
+++ frontend/src/Components/Checkout.js
@@ -1,11 +1,30 @@
 import Navbar from "./Navbar";
-
+import { useState } from "react";
 const Checkout = () => {
+    const[toggle,settoggle]=useState(1);
+
+    const toggletab = (index) => {
+        setToggleState(index);
+      };
+
     return (  
         <div>
             <Navbar />
-            <div className="grid ">
-
+            <div className=" ">
+                <div className="flex">
+                <button
+          className={toggle === 1 ? "border-2 border-solid active:bg-royal active:text-palete focus:bg-royal focus:text-palete " : "border-2 border-solid"}
+          onClick={() => toggletab(1)}
+        >
+          Le panier
+        </button>
+        <button
+          className={toggle === 2 ? "border-2 border-solid active:bg-royal active:text-palete focus:bg-royal focus:text-palete " : "border-2 border-solid"}
+          onClick={() => toggletab(2)}
+        >
+          Les information
+        </button>
+                </div>
             </div>
         </div>
     );
