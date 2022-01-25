cbfb620f007251e75357637d03472ce3492f41e0
mekkaoui7 <ahmedmike350@gmail.com>
2022-01-25 09:46:35 +0000

    rani jay nchouf

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -12,8 +12,8 @@ const Home = () => {
   return ( 
    
   <div>
-     {<Navbar />}
-     <div className=' font-monteserrat'>
+     {<Navbar />} 
+     <div className='font-monteserrat'>
        <img className='h-[calc(100vh-4rem)] w-[calc(100%)] relative object-cover' src={hero} alt="" />
        <h1 className='absolute top-1/3 left-1/2 transform -translate-x-1/2 text-3xl sm:hidden text-palete'>Bienvenue De Votre Boutique </h1>
        <h1 className='hidden sm:block sm:text-4xl lg:text-6xl  absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-palete'>Bienvenue  </h1>
