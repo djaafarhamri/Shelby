c57caa3a9880acc46888f754a9abeb8075bc4c86
mekkaoui7 <ahmedmike350@gmail.com>
2022-01-26 08:05:18 +0000

    search

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -16,7 +16,7 @@ const Navbar = () => {
   const nav = useNavigate()
   return (
     <div className='bg-royal'>
-    <div className="flex bg-royal  justify-around text-palete ">
+    <div className="flex md:flex-auto bg-royal  justify-around text-palete ">
       <div className=" self-start   py-3">
       <svg onClick={()=>isopen(!open)} className="w-8 h-8 relative " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
       { open && <div className='flex flex-col absolute left-0 z-10 h-screen bg-royal w-48 mt-4 '>
@@ -44,14 +44,14 @@ const Navbar = () => {
        </div> }
       
       </div>
-      <input className='hidden h-10 mt-3 ml-4 md:block rounded-xl font-monteserrat text-black text-xl py-1 px-1 bg-gray' type="text" placeholder='Recherche...' />
+      <input className='hidden h-10 mt-3 ml-4 md:block rounded-xl  font-monteserrat text-black text-xl py-1 px-1 bg-gray' type="text" placeholder='Recherche...' />
 
-      <div className="flex-auto flex place-content-center ">
+      <div className="flex-auto flex place-content-center md:mr-5 ">
         <p className=" py-3 font-yellow-tail text-4xl hidden sm:block">Shelby</p>
         <img src={logo} alt="" />
         <p className='py-3 font-yellow-tail text-4xl hidden sm:block '>Boutique</p>
       </div>
-      <div className="flex-auto flex justify-end py-3">
+      <div className="md:flex-initial md:w-32 flex justify-end py-3">
       
          
         <img 
