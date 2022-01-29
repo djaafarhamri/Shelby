d1b41bf47cf27bcc6a4cfc3f41ecf3f75f5fe331
djaafarhamri <hamridjaafar12@gmail.com>
2022-01-29 23:24:50 +0100

    vercel

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+console.log('running');
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
@@ -56,21 +57,20 @@ app.use(express.json());
 // };
 // app.use(cors(corsOptions));
 
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend", "build")));
-  
-  app.get("*", (req, res) => {
-    console.log("ddddddddddddddddd");
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
-}
 app.use(authRoute);
 app.use(productRoute);
 app.use(customerRoute);
 app.use(soldRoute);
 app.use(caisseRoute);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend", "build")));
+
+  app.get("*", (req, res) => {
+    console.log("ddddddddddddddddd");
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 server.listen(PORT, () => {
   console.log("listening on PORT : ", PORT);
 });
