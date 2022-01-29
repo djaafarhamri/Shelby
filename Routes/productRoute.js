cade685006fb6a3d7ffbad213f2ddbb444ede7c4
djaafarhamri <hamridjaafar12@gmail.com>
2022-01-29 10:43:08 +0100

    final

--- Routes/productRoute.js
+++ Routes/productRoute.js
@@ -7,7 +7,7 @@ const { v4 } = require('uuid')
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
-        cb(null, 'uploads/')
+        cb(null, './uploads/')
     },
     filename: function(req, file, cb) {
         cb(null, v4() + '-' + file.originalname)
